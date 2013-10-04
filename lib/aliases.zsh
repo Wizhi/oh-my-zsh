a5a6f678055e033ca60dce70feb36a7b1e99fd9b
Simon <xwizhi@gmail.com>
2013-10-04 15:59:33 +0200

    Added cls alias

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -30,3 +30,6 @@ alias whois="whois -h whois-servers.net"
 
 # ROT13-encode text. Works for decoding, too! ;)
 alias rot13='tr a-zA-Z n-za-mN-ZA-M'
+
+# Old habbits are hard to kill
+alias cls=clear
